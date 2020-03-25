c19dbeeed0653c5fc82782a03317e7f10d9c3205
Sam Perlmutter <sam.perlmutter227@gmail.com>
2020-03-25 12:08:03 -0400

    Changed slash command to /golem

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -5,8 +5,8 @@ use crate::slack::{ SlackSlashCommand, SlackError };
 
 #[post("/", format = "application/x-www-form-urlencoded", data = "<slack_req>")]
 pub fn index(conn: StrikesDbConn, slack_req: Result<SlackSlashCommand, SlackError>) -> String {
-    match slack_req.as_ref().map(|msg| (msg.command.as_str(), msg)) {
-        Ok(("/strikes", slack_msg)) => strikes::auth_strikes(conn, slack_msg).unwrap_or_else(|e| e.to_string()),
+    match slack_req.as_ref().map(|msg| (msg.text.split_whitespace().nth(0).unwrap(), msg)) {
+        Ok(("strikes", slack_msg)) => strikes::auth_strikes(conn, slack_msg).unwrap_or_else(|e| e.to_string()),
         Ok(_) => SlackError::InternalServerError("Slash command parsing error".to_string()).to_string(),
         Err(err) => err.to_string()
     }
