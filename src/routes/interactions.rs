1a34be359c73782ca0f6fb3a4803b5f17d738fc5
Sam Perlmutter <sam.perlmutter227@gmail.com>
2020-03-30 13:14:49 -0400

    Renamed all instances of add_brother to add_strike

--- src/routes/interactions.rs
+++ src/routes/interactions.rs
@@ -9,7 +9,7 @@ use crate::db::excusability::Excusability;
 use crate::db::offense::Offense;
 
 pub fn send_add_strike_modal<'a>(slack_msg: &SlackSlashCommand, auth_token: State<'a, SlackAuthToken>) -> SlackResult {
-    let modal_json: Value = serde_json::from_str(include_str!("../json/add-brother-modal.json"))?;
+    let modal_json: Value = serde_json::from_str(include_str!("../json/add-strike-modal.json"))?;
     let body = json! ({
         "trigger_id": slack_msg.trigger_id,
         "view": modal_json
@@ -34,15 +34,15 @@ pub fn send_add_strike_modal<'a>(slack_msg: &SlackSlashCommand, auth_token: Stat
     Ok(SlackResponse::None)
 }
 
-pub fn receive_add_strike_modal<'a>(conn: StrikesDbConn, view_payload: ViewPayload) -> SlackResult {
+pub fn receive_add_strike_modal<'a>(conn: StrikesDbConn, view_payload: &ViewPayload) -> SlackResult {
     if !view_payload.brother.can_act {
         return Err(SlackError::Unauthorized);
     }
 
-    let excusability = view_payload.values["add_brother_excusability_block"]["add_brother_excusability"]["selected_option"]["value"].as_str().unwrap().parse::<Excusability>()?;
-    let offense = view_payload.values["add_brother_offense_block"]["add_brother_offense"]["selected_option"]["value"].as_str().unwrap().parse::<Offense>()?;
-    let reason = view_payload.values["add_brother_reason_block"]["add_brother_reason"]["value"].as_str().unwrap();
-    let brother_id = view_payload.values["add_brother_target_block"]["add_brother_target"]["selected_user"].as_str().unwrap();
+    let excusability = view_payload.values["add_strike_excusability_block"]["add_strike_excusability"]["selected_option"]["value"].as_str().unwrap().parse::<Excusability>()?;
+    let offense = view_payload.values["add_strike_offense_block"]["add_strike_offense"]["selected_option"]["value"].as_str().unwrap().parse::<Offense>()?;
+    let reason = view_payload.values["add_strike_reason_block"]["add_strike_reason"]["value"].as_str().unwrap();
+    let brother_id = view_payload.values["add_strike_target_block"]["add_strike_target"]["selected_user"].as_str().unwrap();
 
     let strike = InsertableStrike {
         excusability,
