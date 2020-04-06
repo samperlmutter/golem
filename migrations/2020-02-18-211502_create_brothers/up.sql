404de43a4ce719206a8ab93e97b656e5d30fad14
Sam Perlmutter <sam.perlmutter227@gmail.com>
2020-04-06 11:10:38 -0400

    Added more dummy data

--- migrations/2020-02-18-211502_create_brothers/up.sql
+++ migrations/2020-02-18-211502_create_brothers/up.sql
@@ -9,3 +9,7 @@ CREATE TABLE brothers (
 INSERT INTO brothers (slack_id, can_strike, can_reset, `name`) VALUES ("UN4DNTW5S", 1, 1, "Sam");
 INSERT INTO brothers (slack_id, can_strike, can_reset, `name`) VALUES ("UN4DNTW5T", 0, 1, "Ethan");
 INSERT INTO brothers (slack_id, can_strike, can_reset, `name`) VALUES ("UN4DNTW5U", 0, 0, "Danny");
+INSERT INTO brothers (slack_id, can_strike, can_reset, `name`) VALUES ("UN4DNTW5V", 0, 0, "Gavin");
+INSERT INTO brothers (slack_id, can_strike, can_reset, `name`) VALUES ("UN4DNTW5W", 0, 0, "Noah");
+INSERT INTO brothers (slack_id, can_strike, can_reset, `name`) VALUES ("UN4DNTW5X", 0, 0, "Aaron");
+INSERT INTO brothers (slack_id, can_strike, can_reset, `name`) VALUES ("UN4DNTW5Y", 0, 0, "Mitchell");
--- migrations/2020-04-05-223322_points/up.sql
+++ migrations/2020-04-05-223322_points/up.sql
@@ -9,5 +9,6 @@ CREATE TABLE points (
 INSERT INTO points (amount, reason, brother_id) VALUES (20, "doing something sick", "UN4DNTW5S");
 INSERT INTO points (amount, reason, brother_id) VALUES (40, "doing something extra sick", "UN4DNTW5S");
 INSERT INTO points (amount, reason, brother_id) VALUES (-100, "doing something very not sick", "UN4DNTW5S");
+INSERT INTO points (amount, reason, brother_id) VALUES (-100, "doing something very not sick", "UN4DNTW5X");
 INSERT INTO points (amount, reason, brother_id) VALUES (-100, "doing something very very not sick", "UN4DNTW5U");
 INSERT INTO points (amount, reason, brother_id) VALUES (100, "being himself", "UN4DNTW5T");
